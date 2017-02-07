ecbef8620f23c36fd226653214d98af13bedc614
varunv997 <varunv997@outlook.com>
2017-02-07 20:16:16 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,88 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function() {
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   $('#intro-slider').flexslider({
+      animation: 'fade',
+      controlNav: false,
+   });
+
+/*----------------------------------------------------*/
+/*	gmaps
+------------------------------------------------------*/
+
+   var map;
+
+   // main directions
+   map = new GMaps({
+      el: '#map', lat: 12.8236, lng: 80.0442, zoom: 18, zoomControl : true,
+      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
+   });
+
+   // add address markers
+   map.addMarker({ lat: 12.8236, lng: 80.0442, title: 'SRM UAV',
+   infoWindow: { content: '<p>Directorate of Research, SRM University, Kattankulathur, Kancheepuram Dist, Tamil Nadu-603203, India</p>' } });
+
+/*----------------------------------------------------*/
+/*	contact form
+------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+
+      return false;
+
+   });
+
+
+});
+
+
+
+
+
+
+
+
